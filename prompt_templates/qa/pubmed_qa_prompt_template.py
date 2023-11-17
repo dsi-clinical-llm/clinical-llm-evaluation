69512fd5e85d453fbb0fce3aa383c9d64b95a515
Chao Pang <ChaoPang229@gmail.com>
2023-11-17 15:19:15 -0500

    changed the wording the prompts to make the tone of the instruction stronger

--- prompt_templates/qa/pubmed_qa_prompt_template.py
+++ prompt_templates/qa/pubmed_qa_prompt_template.py
@@ -1,22 +1,23 @@
-PUBMED_QA_PROMPT_TEMPLATE_BASE = '''Instruction: Answer the question based on the abstract by simply choosing one 
-of the following options. Your response can only contain the following JSON format: 
+PUBMED_QA_PROMPT_TEMPLATE_BASE = '''
+Instruction: Answer the question based on the abstract by simply choosing one of the following options. Your response must only contain the following JSON format: 
 {
     "correct_option" : "correct option from given options", 
     "correct_option_index" : "index of the correct option"
 }
+ 
 
 Question: {{question}}
 Abstract: {{abstract}}
 
 Options: 
-0. no
-1. yes
-2. maybe
+0: no
+1: yes
+2: maybe
 '''
 
 PUBMED_QA_PROMPT_TEMPLATE_BASE_V1 = '''
 Instruction: As a skilled medical domain expert, you are tasked to analyze multiple-choice questions, and select the correct answer. 
-Your response can only contain the following JSON format: 
+Your response must only contain the following JSON format: 
 {
     "correct_option" : "correct option from given options", 
     "correct_option_index" : "index of the correct option"
@@ -26,15 +27,15 @@ Abstract: {{abstract}}
 Question: {{question}}
 
 Options: 
-0. no
-1. yes
-2. maybe
+0: no
+1: yes
+2: maybe
 '''
 
 PUBMED_QA_PROMPT_TEMPLATE_COT_V1 = '''
 Instruction: Answer the multi-choice question based on the abstract. 
 First, you need to extract evidence from the text related to this question. Then you need to choose one of the following options based on the evidence field 
-Your response can only contain the following JSON format: 
+Your response must only contain the following JSON format: 
 {
     "evidence_field" : "your evidence",
     "correct_option" : "correct option from given options", 
@@ -45,7 +46,7 @@ Question: {{question}}
 Abstract: {{abstract}}
 
 Options: 
-0. no
-1. yes
-2. maybe
+0: no
+1: yes
+2: maybe
 '''
