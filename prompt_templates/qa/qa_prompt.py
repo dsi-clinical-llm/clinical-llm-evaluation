684dec835632e5ca29d2f51cda36527055a95d01
niuyizhan <niuyizhan0405@gmail.com>
2023-12-02 00:43:58 -0500

    Added Open-Ended Evaluation
    not implemented hallucination evaluation for open-ended evaluation yet

---
+++ prompt_templates/qa/qa_prompt.py
@@ -0,0 +1,141 @@
+from jinja2 import Template, Environment
+
+from utils.utils import remove_illegal_chars, extract_from_json_response
+from prompt_templates.prompt_abstract import Prompt
+from prompt_templates.qa.pubmed_qa_prompt_template import PUBMED_QA_PROMPT_TEMPLATE_JSON_V3, \
+    PUBMED_QA_PROMPT_TEMPLATE_JSON_V1, PUBMED_QA_PROMPT_TEMPLATE_JSON_V2, PUBMED_QA_PROMPT_TEMPLATE_BASE_V1
+from prompt_templates.qa.medquad_qa_prompt_template import MEDQUAD_QA_PROMPT_TEMPLATE_V1, MEDQUAD_QA_PROMPT_TEMPLATE_BASE_V1
+
+ENVIRONMENT = Environment()
+
+
+class PubmedQuestionAnswerPromptJsonV1(Prompt):
+    # dict to map answer to an integer
+    answer_idx_mapping = {'no': 0, 'yes': 1, 'maybe': 2, 'unknown': 3}
+    idx_answer_mapping = {v: k for k, v in answer_idx_mapping.items()}
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(PUBMED_QA_PROMPT_TEMPLATE_JSON_V1)
+
+    def extract_answer(
+            self
+    ):
+        answer = extract_from_json_response(
+            model_response=self.model_response,
+            field='correct_option',
+            default_value='unknown'
+        )
+
+        if answer == 'unknown':
+            correct_option_index = extract_from_json_response(
+                model_response=self.model_response,
+                field='correct_option_index',
+                default_value='unknown'
+            )
+            if correct_option_index.isnumeric():
+                answer = self.idx_answer_mapping.get(int(correct_option_index), 'unknown')
+
+        if answer == 'unknown':
+            cleaned_model_response = self.model_response.strip().lower()
+            # first try to convert all the integer to the label
+            if cleaned_model_response.isnumeric():
+                if int(cleaned_model_response) in self.idx_answer_mapping:
+                    answer = self.idx_answer_mapping[int(cleaned_model_response)]
+            else:
+                answer = cleaned_model_response.split(' ')[0]
+
+        return remove_illegal_chars(answer)
+
+    @staticmethod
+    def map_answer(answer):
+        return PubmedQuestionAnswerPromptJsonV1.answer_idx_mapping.get(answer.lower().strip(), 3)
+
+
+class PubmedQuestionAnswerPromptJsonV2(PubmedQuestionAnswerPromptJsonV1):
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(PUBMED_QA_PROMPT_TEMPLATE_JSON_V2)
+
+
+class PubmedQuestionAnswerPromptJsonV3(PubmedQuestionAnswerPromptJsonV1):
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(PUBMED_QA_PROMPT_TEMPLATE_JSON_V3)
+
+
+class PubmedQuestionAnswerPromptBaseV1(Prompt):
+    # dict to map answer to an integer
+    answer_mapping = {'no': 0, 'yes': 1, 'maybe': 2, 'unknown': 3}
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(PUBMED_QA_PROMPT_TEMPLATE_BASE_V1)
+
+    def extract_answer(
+            self
+    ):
+        if self.model_response:
+            parsed_answer = self.model_response.lower()
+            parsed_answer = parsed_answer.replace('answer', '').replace('option', '')
+            # Take the first word from the answer
+            final_answer = remove_illegal_chars(parsed_answer.split(' ')[-1])
+        else:
+            final_answer = 'unknown'
+
+        return final_answer
+
+    @staticmethod
+    def map_answer(answer):
+        return PubmedQuestionAnswerPromptJsonV1.answer_idx_mapping.get(answer.lower().strip(), 3)
+
+
+
+
+class MedQuADQuestionAnswerPromptV1(Prompt):
+    def __init__(
+            self,
+            ground_truth: str,
+            record_id: str = None,
+    ):
+        self.ground_truth = ground_truth
+        self.record_id = record_id
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(MEDQUAD_QA_PROMPT_TEMPLATE_V1)
+
+    @staticmethod
+    def get_base_prompt_class() -> Prompt:
+        return MedQuADQuestionAnswerPromptV1
+
+    def extract_answer(
+            self
+    ):
+        return self.model_response
+    
+
+
+'''
+class MedQuADQuestionAnswerPromptJsonV2(MedQuADQuestionAnswerPromptJsonV1):
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(MEDQUAD_QA_PROMPT_TEMPLATE_JSON_V2)
+
+'''
+class MedQuADQuestionAnswerPromptBaseV1(Prompt):
+    def __init__(
+            self,
+            ground_truth: str,
+            record_id: str = None,
+    ):
+        self.ground_truth = ground_truth
+        self.record_id = record_id
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(MEDQUAD_QA_PROMPT_TEMPLATE_BASE_V1)
+
+    @staticmethod
+    def get_base_prompt_class() -> Prompt:
+        return MedQuADQuestionAnswerPromptV1
+
+    def extract_answer(
+            self
+    ):
+        return self.model_response
