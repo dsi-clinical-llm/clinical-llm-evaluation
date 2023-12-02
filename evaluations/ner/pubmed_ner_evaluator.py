058c57005c6436b0f67fec4a45792b91d12cd7f9
Chao Pang <ChaoPang229@gmail.com>
2023-12-02 10:59:35 -0500

    added the pubmed_ner evaluator to the framework

---
+++ evaluations/ner/pubmed_ner_evaluator.py
@@ -0,0 +1,38 @@
+from typing import List
+
+import evaluate
+
+from prompt_templates.prompt_abstract import Prompt
+from evaluations.causal_llm_evaluators import CausalLanguageModelEvaluator
+from prompt_templates.ner.pubmed_ner.pubmed_ner_prompt import PubmedNameEntityRecognition
+
+
+class PubmedNameEntityRecognitionEvaluator(CausalLanguageModelEvaluator):
+
+    def get_prompt_classes(self) -> List[Prompt]:
+        return [
+            PubmedNameEntityRecognition
+        ]
+
+    def generate_prompts(
+            self,
+            record,
+            few_shot_records
+    ) -> List[Prompt]:
+        identifier = record['abstract_number'] if 'abstract_number' in record else None
+        title = record['title']
+        abstract = record['abstract']
+        context = title + " " + abstract
+        prompts = []
+        for prompt_class in self.get_prompt_classes():
+            prompt = prompt_class(
+                ground_truth='',
+                record_id=identifier,
+                data={'context': context}
+            )
+            prompts.append(prompt)
+
+        return prompts
+
+    def get_metrics(self) -> dict:
+        return {}
