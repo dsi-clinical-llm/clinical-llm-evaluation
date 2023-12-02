7c731cd5e23d4fad3627da7f948a9fc13f171f5d
niuyizhan <niuyizhan0405@gmail.com>
2023-12-02 00:43:58 -0500

    Added Open-Ended Evaluation
    not implemented hallucination evaluation for open-ended evaluation yet

--- prompt_templates/qa/qa_prompt.py
+++ prompt_templates/qa/qa_prompt.py
@@ -4,6 +4,8 @@ from utils.utils import remove_illegal_chars, extract_from_json_response
 from prompt_templates.prompt_abstract import Prompt
 from prompt_templates.qa.pubmed_qa_prompt_template import PUBMED_QA_PROMPT_TEMPLATE_JSON_V3, \
     PUBMED_QA_PROMPT_TEMPLATE_JSON_V1, PUBMED_QA_PROMPT_TEMPLATE_JSON_V2, PUBMED_QA_PROMPT_TEMPLATE_BASE_V1
+from prompt_templates.qa.medquad_qa_prompt_template import MEDQUAD_QA_PROMPT_TEMPLATE_V1, \
+    MEDQUAD_QA_PROMPT_TEMPLATE_BASE_V1
 
 ENVIRONMENT = Environment()
 
@@ -94,3 +96,56 @@ class PubmedQuestionAnswerPromptBaseV1(Prompt):
     @staticmethod
     def map_answer(answer):
         return PubmedQuestionAnswerPromptJsonV1.answer_idx_mapping.get(answer.lower().strip(), 3)
+
+
+class MedQuADQuestionAnswerPromptV1(Prompt):
+    def __init__(
+            self,
+            ground_truth: str,
+            record_id: str = None,
+    ):
+        self.ground_truth = ground_truth
+        self.record_id = record_id
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(MEDQUAD_QA_PROMPT_TEMPLATE_V1)
+
+    @staticmethod
+    def get_base_prompt_class() -> Prompt:
+        return MedQuADQuestionAnswerPromptV1
+
+    def extract_answer(
+            self
+    ):
+        return self.model_response
+
+
+'''
+class MedQuADQuestionAnswerPromptJsonV2(MedQuADQuestionAnswerPromptJsonV1):
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(MEDQUAD_QA_PROMPT_TEMPLATE_JSON_V2)
+
+'''
+
+
+class MedQuADQuestionAnswerPromptBaseV1(Prompt):
+    def __init__(
+            self,
+            ground_truth: str,
+            record_id: str = None,
+    ):
+        self.ground_truth = ground_truth
+        self.record_id = record_id
+
+    def get_prompt_template(self) -> Template:
+        return ENVIRONMENT.from_string(MEDQUAD_QA_PROMPT_TEMPLATE_BASE_V1)
+
+    @staticmethod
+    def get_base_prompt_class() -> Prompt:
+        return MedQuADQuestionAnswerPromptV1
+
+    def extract_answer(
+            self
+    ):
+        return self.model_response
