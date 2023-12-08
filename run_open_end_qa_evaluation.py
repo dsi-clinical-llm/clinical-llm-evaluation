c1268286acf32bf606e045530e443653b793e3d6
niuyizhan <58444512+niuyizhan@users.noreply.github.com>
2023-12-08 00:13:07 -0500

    add_open_ended_runner

---
+++ run_open_end_qa_evaluation.py
@@ -0,0 +1,98 @@
+from multiprocessing import Pool
+from evaluations import MedQuADQaEvaluator
+from evaluations.causal_llm_evaluators import CausalLanguageModelEvaluator
+from evaluations.hallucination.medquad_hallucination_evaluator import MedQuADQaHallucinationEvaluator
+from models import get_model_wrapper
+
+from datasets import load_dataset
+from utils.utils import create_train_test_partitions, get_all_args
+from utils.evaluation_args import add_main_arguments
+
+TEST_SIZE = 0.2
+RANDOM_SEED = 42
+
+
+def create_argparser():
+    import argparse
+    parser = argparse.ArgumentParser(description='Open Ended QA evaluation')
+    add_main_arguments(parser)
+    parser.add_argument(
+        '--n_of_shots',
+        dest='n_of_shots',
+        action='store',
+        type=int,
+        default=0,
+        required=False
+    )
+    return parser.parse_args()
+
+
+def parallel_evaluation(
+        process_id,
+        parsed_args,
+        sub_dataset,
+        evaluator_class: CausalLanguageModelEvaluator,
+        *positional_args,
+        **kwargs
+):
+    model_wrapper_class = get_model_wrapper(parsed_args.model_choice)
+    required_args, default_args = get_all_args(model_wrapper_class)
+
+    arg_dict = {}
+    for required_arg in required_args:
+        if not hasattr(parsed_args, required_arg):
+            raise RuntimeError(f'{required_arg} is a required argument for {parsed_args.model_choice}')
+        arg_dict[required_arg] = getattr(parsed_args, required_arg)
+
+    for default_arg in default_args:
+        if hasattr(parsed_args, default_arg):
+            arg_dict[default_arg] = getattr(parsed_args, default_arg)
+
+    arg_dict['max_new_tokens'] = parsed_args.max_new_tokens
+
+    model_wrapper = model_wrapper_class(**arg_dict)
+    evaluator = evaluator_class(
+        dataset=sub_dataset,
+        model=model_wrapper,
+        evaluation_folder=parsed_args.evaluation_folder,
+        n_of_shots=parsed_args.n_of_shots if hasattr(parsed_args, 'n_of_shots') else 0,
+        restore_checkpoint=parsed_args.restore_checkpoint,
+        process_id=process_id,
+        *positional_args,
+        **kwargs
+    )
+    evaluator.evaluate()
+
+
+if __name__ == '__main__':
+
+    args = create_argparser()
+
+    medquad_qa_dataset = load_dataset('lavita/MedQuAD')
+
+    dataset = medquad_qa_dataset['train'].train_test_split(
+        seed=RANDOM_SEED,
+        test_size=TEST_SIZE
+    )
+
+
+    partitioned_datasets = create_train_test_partitions(
+        dataset,
+        n=args.num_of_cores
+    )
+
+    # Get the evaluator
+    medquad_evaluator_class = MedQuADQaHallucinationEvaluator if args.is_hallucination_test else MedQuADQaEvaluator
+
+    pool_tuples = []
+    for i in range(args.num_of_cores):
+        pool_tuples.append(
+            (i, args, partitioned_datasets[i], medquad_evaluator_class)
+        )
+
+    with Pool(processes=args.num_of_cores) as p:
+        p.starmap(parallel_evaluation, pool_tuples)
+        p.close()
+        p.join()
+
+    print('Done')
